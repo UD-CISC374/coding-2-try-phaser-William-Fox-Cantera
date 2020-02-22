4427c2626f63536b4939a43855b61f7f3a967cfd
William-Fox-Cantera <wfcantera@gmail.com>
2020-02-22 12:55:50 -0500

    Updates

---
+++ dev/precache-manifest.6fc706419fdd935598645d4f857fe23b.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0a960f71656855377a840afddda2d7",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devassets/audio/TheOnlyThingTheyFearIsYou.mp3"
+  },
+  {
+    "revision": "86b88d1e4615b77a8b94a93ba82500bf",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devassets/audio/beam.mp3"
+  },
+  {
+    "revision": "9e59cd5013019ffca2a7c9927a7d6c56",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devassets/citations.md"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devassets/font/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devassets/font/font.xml"
+  },
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devassets/images/background.png"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devassets/images/ship.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devassets/images/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devassets/images/ship3.png"
+  },
+  {
+    "revision": "6be3462bcaa4a183b678800bb66d176c",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devassets/spritesheets/beam.png"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devassets/spritesheets/explosion.png"
+  },
+  {
+    "revision": "ff70e55fbb2b70941d98b3e7ab1a01aa",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devassets/spritesheets/player.png"
+  },
+  {
+    "revision": "05427110fd2c2de1d4738b31496a53c9",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devassets/spritesheets/power-up.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devassets/spritesheets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devassets/spritesheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devassets/spritesheets/ship3.png"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devfavicon.ico"
+  },
+  {
+    "revision": "50b8b1cc0459fe2a990c",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devgen.main.ab8161446894364ba687.bundle.js"
+  },
+  {
+    "revision": "7e3abc0967d71642756e",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devgen.vendors.bbe9474c65e057812275.bundle.js"
+  },
+  {
+    "revision": "f3b1ff567a0c4b624e7965e2068a8ff5",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devicons/icons-192.png"
+  },
+  {
+    "revision": "940dadf1b7379fa02e546441b85f487a",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devicons/icons-512.png"
+  },
+  {
+    "revision": "a81ea5a11bef08c82edc17dcd71050b7",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devindex.html"
+  },
+  {
+    "revision": "59bbe9abd4e0dc9e720461300f545959",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-William-Fox-Cantera/devsw.js"
+  }
+]);
--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-importScripts("/will-game/devprecache-manifest.703c9cd2939c71351b07d9ec60033ee1.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/coding-2-try-phaser-William-Fox-Cantera/devprecache-manifest.6fc706419fdd935598645d4f857fe23b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
